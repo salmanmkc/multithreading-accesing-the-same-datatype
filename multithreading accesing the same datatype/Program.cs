b70395a62ffda0f1cbdc4ed1cb7f9a68b9e5e1f8
SalmanMKC <32169182+salmanmkc@users.noreply.github.com>
2019-02-06 00:37:32 +0000

    Initial commit

---
+++ multithreading accesing the same datatype/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace multithreading_accesing_the_same_datatype
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int value = 0;
+            object _lock = new object();
+            Task t = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    lock (_lock)
+                    {
+                        value++;
+                    }
+
+                }
+            });
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (_lock)
+                {
+                    value--;
+                }
+
+            }
+
+            t.Wait();
+            Console.WriteLine(value);
+        }
+    }
+}
